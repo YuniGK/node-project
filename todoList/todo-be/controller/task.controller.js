e5dee46654a11bfb0739845933ba5df24a843df8
kim yunjung <1106qwe@hanmail.net>
2024-10-15 21:04:10 +0900

    update / delete

--- todoList/todo-be/controller/task.controller.js
+++ todoList/todo-be/controller/task.controller.js
@@ -30,4 +30,30 @@ taskController.getTask = async (req, res) => {
     }
 };
 
+taskController.putTask = async (req, res) => {
+    try {
+        const taskId = parseInt(req.params.id);
+
+        const taskUpdate = await Task.updateOne({ taskId }, {isComplete: req.body.isComplete}); 
+        //const taskUpdate = await Task.updateOne({ task: 'test' }, {isComplete: req.body.isComplete});
+
+        res.status(200).json({status : 'ok', data : taskUpdate});
+    } catch (error) {
+        res.status(400).json({status : 'fail', error : err});
+    }
+};
+
+taskController.deleteTask = async (req, res) => {
+    try {
+        const taskId = parseInt(req.params.id);
+
+        const taskDelete = await Task.deleteOne({ taskId });
+        //const taskDelete = await Task.deleteMany({ isComplete : true });
+
+        res.status(200).json({status : 'ok', data : taskDelete});
+    } catch (error) {
+        res.status(400).json({status : 'fail', error : err});
+    }
+};
+
 module.exports = taskController;
