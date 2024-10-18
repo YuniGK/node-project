54e23bb8365885c67310c7b524127251318cd07d
kim yunjung <1106qwe@hanmail.net>
2024-10-18 23:53:54 +0900

    배포전 설정

--- todoList/todo-fe-login/src/utils/api.js
+++ todoList/todo-fe-login/src/utils/api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  //baseURL: `${process.env.REACT_APP_BACKEND_PROXY}/api`,
-  baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`,
+  baseURL: `${process.env.REACT_APP_BACKEND_PROXY}/api`,
+  //baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`,
   headers: {
-    "Content-Type": "application/json",
-    'Accept': 'application/json', 
+    "Content-Type": "application/json", 
     authorization: "Bearer " + localStorage.getItem("token"),
   },
 });
