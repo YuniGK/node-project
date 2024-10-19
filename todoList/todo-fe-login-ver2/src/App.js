606a24cd27cbfaf9a0cd85fd3557a11346ee1187
kim yunjung <1106qwe@hanmail.net>
2024-10-19 14:23:47 +0900

    토큰 넣는 방식 1
    토큰이 있는지 확인하여 이미 토큰이 있을 경우 토큰을 넣어준다.

--- todoList/todo-fe-login-ver2/src/App.js
+++ todoList/todo-fe-login-ver2/src/App.js
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import TodoPage from "./pages/TodoPage";
 import RegisterPage from "./pages/RegisterPage";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PrivateRoute from "./route/PrivateRoute";
 import api from "./utils/api";
 
@@ -14,13 +14,24 @@ function App() {
   const getUser = async() => {
     try {
       //토큰값 읽어오기
-      const token = sessionStorage.getItem('token');
+      const storedToken = sessionStorage.getItem('token');
 
+      //토큰이 있는지 확인하여 이미 토큰이 있을 경우 토큰을 넣어준다.
+      if(storedToken){
+        api.defaults.headers["authorization"] = "Bearer "+storedToken;
+      }
+
+      //토큰을 통해 유저정보를 가져온다.
       const response = api.get("/user/me");
     } catch (error) {
       
     }
   }
+
+  //페이지가 불러왔을때 토큰을 확인하여 보여줄 페이지를 정한다.
+  useEffect(()=>{
+    getUser();
+  }, []);
 
   return (
     <Routes>
