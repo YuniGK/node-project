db69bcd7999fbd49150609fb63fa4606a00559f6
kim yunjung <1106qwe@hanmail.net>
2024-10-19 12:56:56 +0900

    Protected Route / Private Route 설정

--- todoList/todo-fe-login-ver2/src/App.js
+++ todoList/todo-fe-login-ver2/src/App.js
@@ -8,7 +8,14 @@ import RegisterPage from "./pages/RegisterPage";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<TodoPage />} />
+      {/* 로그인 되어, 토큰이 있는 유저만 접속이 가능한 페이지 
+      Protected Route / Private Route 필요
+
+      https://medium.com/@duchanjo/react-router-protected-route-a7d40491045f
+      https://medium.com/@bhairabpatra.iitd/private-routes-in-react-559a7d8d161f
+      */}
+      <Route path="/" element={<PrivateRoute><TodoPage /></PrivateRoute>} />
+
       <Route path="/register" element={<RegisterPage />} />
 
       <Route path="/login" element={<LoginPage />} />
---
+++ todoList/todo-fe-login-ver2/src/route/PrivateRoute.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+//rafce 단축키 기본틀 생성
+const PrivateRoute = () => {
+  return (
+    <div>PrivateRoute</div>
+  )
+}
+
+export default PrivateRoute
