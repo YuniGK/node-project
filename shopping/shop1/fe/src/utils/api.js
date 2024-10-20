2a35ea6b7dd505b518dc72b7c19afc42fa124ea3
kim yunjung <1106qwe@hanmail.net>
2024-10-20 17:53:32 +0900

    배포 셋팅

--- shopping/shop1/fe/src/utils/api.js
+++ shopping/shop1/fe/src/utils/api.js
@@ -2,13 +2,10 @@ import axios from "axios";
 // 상황따라 주소 다름
 const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
 // const PROD_BACKEND = process.env.REACT_APP_PROD_BACKEND;
-// const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
-// console.log("proxy", BACKEND_PROXY);
-
-console.log('LOCAL_BACKEND ', LOCAL_BACKEND)
+const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
 
 const api = axios.create({
-  baseURL: LOCAL_BACKEND,
+  baseURL: BACKEND_PROXY,
   headers: {
     "Content-Type": "application/json",
     authorization: `Bearer ${sessionStorage.getItem("token")}`,
