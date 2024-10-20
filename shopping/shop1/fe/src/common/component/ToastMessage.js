6e77da5b66b947ecda7b281b30c83c4c119eba58
kim yunjung <1106qwe@hanmail.net>
2024-10-20 16:18:04 +0900

    -

--- shopping/shop1/fe/src/common/component/ToastMessage.js
+++ shopping/shop1/fe/src/common/component/ToastMessage.js
@@ -18,7 +18,7 @@ const ToastMessage = () => {
   return (
     <ToastContainer
       position="top-right"
-      autoClose={5000}
+      autoClose={5050}//{5000}
       hideProgressBar={false}
       newestOnTop={false}
       closeOnClick
