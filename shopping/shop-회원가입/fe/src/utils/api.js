ea68427268d7b0cdbcac5837768d942c7f99d696
kim yunjung <1106qwe@hanmail.net>
2024-10-21 22:08:31 +0900

    fe 실행되지 않던 부분 수정

--- shopping/shop-회원가입/fe/src/utils/api.js
+++ shopping/shop-회원가입/fe/src/utils/api.js
@@ -2,16 +2,15 @@ import axios from "axios";
 // 상황따라 주소 다름
 const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
 // const PROD_BACKEND = process.env.REACT_APP_PROD_BACKEND;
-const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
-
+// const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
+// console.log("proxy", BACKEND_PROXY);
 const api = axios.create({
-  baseURL: BACKEND_PROXY,
+  baseURL: `${LOCAL_BACKEND}/api`,
   headers: {
     "Content-Type": "application/json",
     authorization: `Bearer ${sessionStorage.getItem("token")}`,
   },
 });
-
 /**
  * console.log all requests and responses
  */
