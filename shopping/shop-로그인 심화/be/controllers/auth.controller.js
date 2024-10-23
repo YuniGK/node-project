f651c6eafce2995c3ca23770fe2bec56835baa3b
kim yunjung <1106qwe@hanmail.net>
2024-10-23 21:56:51 +0900

    -

--- shopping/shop-로그인 심화/be/controllers/auth.controller.js
+++ shopping/shop-로그인 심화/be/controllers/auth.controller.js
@@ -1,7 +1,12 @@
 const User = require("../models/User");
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
 
 const authController = {};
+
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 authController.loginWithEmail = async (req, res) => {
     try {
@@ -28,5 +33,30 @@ authController.loginWithEmail = async (req, res) => {
     }
 }
 
+authController.authenticate = async (req, res, next) => {
+    try {
+        const tokenString = req.headers.authorization;
+
+        if(!tokenString){
+            throw new Error("토큰이 없습니다.");
+        }
+
+        const token = tokenString.replace("Bearer ", "");
+
+        //토크값 복호화
+        jwt.verify(token, JWT_SECRET_KEY, (error, payload) => {
+            if(error)
+                throw new Error("유효하지 않은 토큰입니다.");
+
+            req.userId = payload._id;
+        });
+
+        //다음으로 넘어간다.
+        next();
+
+    } catch (error) {
+        res.status(400).json({status : "auth fail", message : error.message});
+    }
+}
 
 module.exports = authController;
--- shopping/shop-로그인 심화/be/controllers/user.controller.js
+++ shopping/shop-로그인 심화/be/controllers/user.controller.js
@@ -32,5 +32,21 @@ userController.createUser = async (req, res) => {
     }
 }
 
+userController.getUser = async (req, res) => {
+    try {
+        const {userId} = req;
+
+        const user = await User.findById(userId);
+
+        if(!user){
+            throw new Error("유저가 없습니다.");
+        }
+
+        res.status(200).json({status : "getUser success", user});
+        
+    } catch (error) {
+        res.status(400).json({status : "getUser fail", message : error.message});
+    }
+}
 
 module.exports = userController;
